30db342fee6cca01d99971d454afa444af3be172
sy <2175075194@qq.com>
2026-04-23 16:24:08 +0800

    搜索初始化

---
+++ src/components/form/SearchForm.tsx
@@ -0,0 +1,48 @@
+import React, { useState, type ChangeEvent, type FormEvent } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
+type SearchFormProps = {
+  userName: string;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
+};
+const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
+  const [text, setText] = useState(userName);
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!text) {
+      toast("You must input value");
+      return;
+    }
+    setUserName(text);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <form
+      className="flex items-center gap-x-2 w-full lg:w-1/3 mb-8"
+      onSubmit={handleSearch}
+    >
+      <Label htmlFor="search"></Label>
+      <Input
+        id="search"
+        value={text}
+        onChange={handleChange}
+      />
+      <Button
+        type="submit"
+        variant="default"
+        size={"lg"}
+      >
+        提交
+      </Button>
+    </form>
+  );
+};
+
+export default SearchForm;
