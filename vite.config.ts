6614b523c1ce57d226668bbbca8d18c02b53ee1c
sy <2175075194@qq.com>
2026-04-23 16:26:42 +0800

    dev初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // v4 唯一官方插件
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // @/ 别名保留
+    },
+  },
+});
